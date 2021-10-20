a9f8c830015ad757c562e22d3258201fdfbd0f75
Xavier Mejía <xavierams08@gmail.com>
2021-10-19 20:56:44 -0400

    Update Program.cs

--- 9.2 Problems for Champions – Part II/2-Numerical Expression/Program.cs
+++ 9.2 Problems for Champions – Part II/2-Numerical Expression/Program.cs
@@ -6,8 +6,71 @@ namespace _2_Numerical_Expression
     {
         static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            int symbol = int.Parse( Console.ReadLine());
+
+           
+            decimal result = 0;
+            int expresionOperator = '+';
+
+            while (symbol != '=')
+            {
+                symbol = int.Parse(Console.ReadLine());
+            }
+
+            if (symbol == '(')
+            {
+                decimal innerResult = 0;
+                int innerOperator = '+';
+                symbol = int.Parse(Console.ReadLine());
+            }
+            else if (0 <= symbol - '0' && symbol - '0' <= 9)
+            {
+                switch (expresionOperator)
+                {
+                    case '+':
+                        result += symbol - '0';
+                        break;
+                    case '-':
+                        result -= symbol - '0';
+                        break;
+                    case '*':
+                        result *= symbol - '0';
+                        break;
+                    case '/':
+                        result /= symbol - '0';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (symbol == '+' || 
+                     symbol == '-' || 
+                     symbol == '/' || 
+                     symbol == '*' )
+            {
+                expresionOperator = symbol;
+            }
+            switch (expresionOperator)
+            {
+                case '+':
+                    result += inner;
+                    break;
+                case '-':
+                    result -= symbol - '0';
+                    break;
+                case '*':
+                    result *= symbol - '0';
+                    break;
+                case '/':
+                    result /= symbol - '0';
+                    break;
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Main();
         }
     }
 }
